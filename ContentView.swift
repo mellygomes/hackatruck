640276195aec027e04c052b0def04ffc503374f2
Emanuelly Queiroz <133934109+mellygomes@users.noreply.github.com>
2025-04-16 16:02:03 -0300

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             //linha com a foto de perfil
-            HStack {
+            HStack(spacing: 30) {
                 
                 //coluna com a foto de perfil
                 VStack {
@@ -20,19 +20,19 @@ struct ContentView: View {
                         .frame(width: 120)
                 }
                 ////coluna com as informacoes pequenas
-                Spacer()
-                VStack {
+                
+                VStack(spacing: 15) {
                     HStack {
                         //informacoes de followers
-                        VStack {
+                        VStack(spacing: 6) {
                             Text("55")
                             Text("Posts")
                         }
-                        VStack {
+                        VStack(spacing: 6) {
                             Text("152")
                             Text("Followers")
                         }
-                        VStack {
+                        VStack(spacing: 6) {
                             Text("37")
                             Text("Following")
                         }
@@ -40,39 +40,93 @@ struct ContentView: View {
                     
                     HStack {
                         
-                        HStack {
-                            Text("Editar algo sl")
-                                .padding(4)
-                        }
+                        Text("Adicionar imagem ")
+                            .fixedSize(horizontal: false, vertical: true)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(width: 200, height: 25)
+                            .background(Rectangle().fill(Color.white).shadow(radius: 3))
                         .background(Color(.lightGray))
     
                     }
                 }
             }
             
+            Spacer()
+            
             VStack {
                     Text("Nome e sobrenome")
                         .frame(maxWidth: .infinity, alignment: .leading)
+                        .frame(width: 325, height: 15)
+                
                     Text("Lorem ipsum dolor sit amet")
                         .frame(maxWidth: .infinity, alignment: .leading)
-
+                        .frame(width: 325, height: 15)
             }
+            
+            Spacer()
+            
+            HStack {
+                Text("-")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 330, height: 25)
+                    .background(Rectangle().fill(Color.gray).shadow(radius: 3))
+                .background(Color(.lightGray))
+            }
+            .padding()
+            
             
             //icones
-            HStack {
-                Image(systemName: 
-                        "square.grid.3x3.fill")
+            HStack(spacing: 100) {
+                Image(systemName:
+                        "square.grid.3x3")
                 
+                Image(systemName: "square.grid.3x1.below.line.grid.1x2")
                 
-                Image(systemName: "square.grid.3x1.fill.below.line.grid.1x2")
-                
-                Image(systemName: "house.fill")
-                
+                Image(systemName: "house")
                 
             }
             
-            HStack {
+            Spacer()
+            
+            VStack(spacing: 5) {
+                HStack (spacing: 5) {
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                }
                 
+                HStack {
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                }
+                
+                HStack {
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                }
+                
+                HStack {
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                    Rectangle()
+                        .fill(.gray)
+                }
             }
             .padding()
         }
